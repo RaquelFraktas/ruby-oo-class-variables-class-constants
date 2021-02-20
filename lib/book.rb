92af47451b94b514655b31be1f803a5297c8d0d1
github username <github email address>
2021-02-20 14:48:18 -0500

    commit tree 618812672d40ab0b3f9bfae61d92c2cc0dc1b2d5

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = [] 
 
   def initialize(title)
     @title = title
@@ -10,4 +12,12 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre #why did we have to do this in here vs initialize?. -> just preferred with the test specs
+    #if i wanted to call this GENRES array, i would call it by doing Book::GENRES
+  end
+
+
 end
+
